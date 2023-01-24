e6fc7a4fbe7f1eb9e13310b0ede782846d0aa324
Erion Barasuol <erion@Erions-MacBook-Air.local>
2023-01-24 14:43:07 -0300

    package pointers

---
+++ pointers_n_errors/wallet.go
@@ -0,0 +1,34 @@
+package pointers_n_errors
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsuficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return (*w).balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsuficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
